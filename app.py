7091bf676a968758e328fceaa60068321781c571
claudiaalozano <91722847+claudiaalozano@users.noreply.github.com>
2026-05-20 23:20:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import numpy as np
 import plotly.graph_objects as go
 import plotly.express as px
 from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 # ============================================================================
 # CONFIGURACIÓN DE LA PÁGINA
